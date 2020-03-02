0e382c426b322d375bad05de53425b6a1b2a5ceb
Elias Torrente Jr <contato@guarubusca.com>
2020-03-02 16:58:34 -0300

    Check logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -59,6 +59,10 @@ public class UI {
 		System.out.println("Turn: " + chessMatch.getTurn());
 		System.out.print("Waiting player: " + chessMatch.getCurrentPlayer());
 		System.out.println();
+		
+		if(chessMatch.getCheck()) {
+			System.out.println("CHECK!");
+		}
 	}
 	
 	public static void printBoard(ChessPiece[][] pieces) {
